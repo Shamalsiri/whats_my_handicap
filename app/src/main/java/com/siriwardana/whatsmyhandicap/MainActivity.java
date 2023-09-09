f1a8812fac7d77810d356fc032a38aba6261b7a1
Shamal Siriwardana <shamal.v.siriwardana@gmail.com>
2023-09-09 00:32:40 -0400

    [SS][login_page] Base design for the login/register page

--- app/src/main/java/com/siriwardana/whatsmyhandicap/MainActivity.java
+++ app/src/main/java/com/siriwardana/whatsmyhandicap/MainActivity.java
@@ -3,12 +3,17 @@ package com.siriwardana.whatsmyhandicap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button registerBtn, loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
