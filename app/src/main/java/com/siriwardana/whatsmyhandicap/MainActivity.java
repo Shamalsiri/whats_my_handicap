6887c35f47bb711d54429a32b6ce99ff620a28c1
Shamal Siriwardana <shamal.v.siriwardana@gmail.com>
2023-09-09 23:22:20 -0400

    [SS][login_page] Base activity design for main menu and setup game activities

--- app/src/main/java/com/siriwardana/whatsmyhandicap/MainActivity.java
+++ app/src/main/java/com/siriwardana/whatsmyhandicap/MainActivity.java
@@ -2,7 +2,9 @@ package com.siriwardana.whatsmyhandicap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loginBtn = (Button) findViewById(R.id.btn_login);
+        registerBtn = (Button) findViewById(R.id.btn_register);
 
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainMenuActivity.class);
+                MainActivity.this.startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/siriwardana/whatsmyhandicap/NewRoundSetupActivity.java
@@ -0,0 +1,14 @@
+package com.siriwardana.whatsmyhandicap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewRoundSetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_round_setup);
+    }
+}
