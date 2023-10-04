09decf48f61d14594bca76c85bec5e403f5dd425
Shamal Siriwardana <shamal.v.siriwardana@gmail.com>
2023-10-03 22:59:11 -0400

    [SS][New_Round_Setup] Validate inputs, store data and start game

--- app/src/main/java/com/siriwardana/whatsmyhandicap/NewRoundSetupActivity.java
+++ app/src/main/java/com/siriwardana/whatsmyhandicap/NewRoundSetupActivity.java
@@ -2,19 +2,25 @@ package com.siriwardana.whatsmyhandicap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class NewRoundSetupActivity extends AppCompatActivity {
 
     private EditText locationEt, courseNameEt;
     private RadioGroup holesRg;
-    private RadioButton nineRb, eighteenRb, twentySevenRb, thirtySixRb;
+    private RadioButton selectedRb;
     private Button backBtn, startBtn;
+    private String location, courseName;
+    private int holes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,19 +42,75 @@ public class NewRoundSetupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(validateInputs()) {
-                    //store vals
-                    //show popup - enter course info now, or as you go
+                    //store values
+                    storeRoundData();
+
+                    // todo: show popup - enter course info now, or as you go
+
                     //start round
+                    Intent intent = new Intent(NewRoundSetupActivity.this, RoundDataEntryActivity.class);
+                    intent.putExtra("location", location);
+                    intent.putExtra("courseName", courseName);
+                    intent.putExtra("holes", holes);
+                    startActivity(intent);
                 }
             }
         });
 
     }
 
+    private void storeRoundData() {
+        location = locationEt.getText().toString().trim();
+        Log.d("Siri", "Location entered: " + location);
+
+        courseName = courseNameEt.getText().toString().trim();
+        Log.d("Siri", "Course name entered: " + courseName);
+
+        selectedRb = (RadioButton) findViewById(holesRg.getCheckedRadioButtonId());
+        holes = Integer.valueOf(selectedRb.getText().toString());
+        Log.d("Siri", "Number of holes selected: " + holes);
+
+        //todo: Store the data in the db;
+    }
+
     private boolean validateInputs() {
-        //validate edit texts
-        //validate radiobuttons
-        return false;
+        Toast toast = new Toast(NewRoundSetupActivity.this);
+
+        //validate locations
+        if(locationEt.getText().length() < 3) {
+            Log.d("Siri", "Club location validation failed");
+
+            toast.setText("Enter Club Name");
+            toast.show();
+            locationEt.setHintTextColor(Color.RED);
+            locationEt.setHint("Required");
+            return false;
+        }
+
+        //validate course
+        if (courseNameEt.getText().length() < 1) {
+            Log.d("Siri", "Course name empty validation failed");
+
+            toast.setText("Enter Course Name");
+            toast.show();
+            courseNameEt.setHintTextColor(Color.RED);
+            courseNameEt.setHint("Required");
+            return false;
+        }
+
+        //validate radio buttons
+        if (holesRg.getCheckedRadioButtonId() == -1) {
+            Log.d("Siri", "Number of holes for round validation failed");
+
+            // no radio buttons are checked
+            toast.setText("Select Number of Holes");
+            toast.show();
+            return false;
+        }
+
+        Log.d("Siri", "New Round setup successfully started");
+
+        return true;
     }
 
     private void setupButtons() {
@@ -58,13 +120,9 @@ public class NewRoundSetupActivity extends AppCompatActivity {
 
     private void setupRadioButtons() {
 
-        //radio buttons
-        nineRb = (RadioButton) findViewById(R.id.rb_nine);
-        eighteenRb = (RadioButton) findViewById(R.id.rb_eighteen);
-        twentySevenRb = (RadioButton) findViewById(R.id.rb_twenty_seven);
-        thirtySixRb = (RadioButton) findViewById(R.id.rb_thirty_six);
+        // radio buttons
 
-        //radio group
+        // radio groups
         holesRg = (RadioGroup) findViewById(R.id.rg_holes);
     }
 
